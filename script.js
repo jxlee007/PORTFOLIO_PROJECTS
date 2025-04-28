2d4b07ab444ceb49aae435b91de4b61cf8474b4e
jxlee007 <107770093+jxlee007@users.noreply.github.com>
2025-04-28 03:52:36 +0000

    feat: enhance footer and social links with improved styling and animations

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
+let locoScrollInstance;
+
 locoScroll = () => {
     gsap.registerPlugin(ScrollTrigger);
 
     // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
-    const locoScroll = new LocomotiveScroll({
+    const locoScrollInstance = new LocomotiveScroll({
         el: document.querySelector("#main"),
         smooth: true
     });
     // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-    locoScroll.on("scroll", ScrollTrigger.update);
+    locoScrollInstance.on("scroll", ScrollTrigger.update);
 
     // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
     ScrollTrigger.scrollerProxy("#main", {
         scrollTop(value) {
-            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+            return arguments.length ? locoScrollInstance.scrollTo(value, 0, 0) : locoScrollInstance.scroll.instance.scroll.y;
         }, // we don't have to define a scrollLeft because we're only scrolling vertically.
         getBoundingClientRect() {
             return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
@@ -23,13 +25,23 @@ locoScroll = () => {
     });
 
     // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    ScrollTrigger.addEventListener("refresh", () => locoScrollInstance.update());
 
     // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
     ScrollTrigger.refresh();
 
 };
 locoScroll();
+
+document.getElementById('go').addEventListener('click', () => {
+    if (locoScrollInstance) {
+        locoScrollInstance.scrollTo(0, {
+            duration: 1000, // duration in ms
+            easing: [0.25, 0.0, 0.35, 1.0] // optional easing (cubic-bezier)
+        });
+    }
+});
+
 
 
 
@@ -204,9 +216,9 @@ animfooter = () => {
         scrollTrigger: {
             trigger: "#footer",
             scroller: "#main",
-            start: "top 70%",
-            end: "top 55%",
-            markers: true,
+            start: "top 90%",
+            end: "top 80%",
+            // markers: true,
             scrub: 2,
         }
     })
