7b861cfce923e4a049a00150b728999b08c4c796
jxlee007 <jimmyfalcon@gmail.com>
2025-11-07 21:57:14 +0530

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,6 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
 //   site: 'https://your-domain.com',
+  site: 'https://jxlee007.github.io',
+  // base: '',
 });
