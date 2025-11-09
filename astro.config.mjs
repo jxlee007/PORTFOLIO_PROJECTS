d3e4f0e0d006f19ea89d737cdb02ac6c3c295fe5
jxlee007 <jimmyfalcon@gmail.com>
2025-11-09 18:29:25 +0530

    SSOT - work

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://jxlee007.github.io',
+  site: 'https:/jxlee007.github.io',
   integrations: [tailwind()],
 //   site: 'https://your-domain.com',
   // base: '',
